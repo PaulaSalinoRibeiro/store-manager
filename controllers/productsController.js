4ff257bc03c60c94eb9aafc968f154ca89b42a02
Paula <paulasalinor@gmail.com>
2022-06-29 14:52:20 -0300

    feat getById productsController add function that search a product in daabase by id

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -9,6 +9,20 @@ const getAll = async (_req, res, next) => {
   }
 };
 
+const getById = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const [data] = await ProductsService.getById(id);
+   
+    if (!data) return res.status(404).json({ message: 'Product not found' });
+
+    res.status(200).json(data);
+  } catch (err) {
+    next(err);
+  }
+};
+
 module.exports = {
   getAll,
+  getById,
 };

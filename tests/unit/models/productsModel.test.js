93d8a9e8569f4ee44e0dd7d1b052308678188048
Paula <paulasalinor@gmail.com>
2022-06-29 16:18:48 -0300

    feat: test layer model add test to fuctions getAll and getById

---
+++ tests/unit/models/productsModel.test.js
@@ -0,0 +1,79 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const connection = require('../../../models/connection');
+const ProductsModel = require('../../../models/productsModel');
+
+describe('Test productsModel', () => {
+
+  describe('check if find all products', () => {
+
+    const produtcs = [
+      {
+        "id": 1,
+        "name": "Martelo de Thor",
+      },
+      {
+        "id": 2,
+        "name": "Traje de encolhimento",
+      }
+    ];
+
+    beforeEach(async () => {
+      await sinon.stub(connection, 'execute').resolves(produtcs)
+    });
+
+    afterEach(async () => await connection.execute.restore());
+    
+    it('should be a object', async () => {
+      const result = await ProductsModel.getAll();
+      expect(result).to.be.a('object');
+    });
+  })
+
+  describe('check if find product by id', () => {
+    
+    describe('where find product', () => {
+
+      const product = [{}];
+
+      beforeEach(async () => {
+        await sinon.stub(connection, 'execute').resolves(product);
+      });
+
+      afterEach(async () => await connection.execute.restore());
+
+      it('should be a empty object', async () => {
+        const result = await ProductsModel.getById('a')
+        expect(result).to.be.a('object')
+      });
+    })
+
+    describe('where not find product', () => {
+
+      const productByID = [{
+        "id": 1,
+        "name": "Martelo de Thor",
+      }];
+
+      beforeEach(async () => {
+        await sinon.stub(connection, 'execute').resolves(productByID);
+      });
+
+      afterEach(async () => await connection.execute.restore());
+
+      it('should be a object with a product', async () => {
+        const result = await ProductsModel.getById(1)
+        expect(result).to.be.a('object')
+      });
+
+      it('should have a id and name with a product', async () => {
+        const result = await ProductsModel.getById(1)
+        expect(result).to.deep.equal({
+          "id": 1,
+          "name": "Martelo de Thor",
+        })
+      });
+    });
+  });
+});

45c8603af03c96c7ea0a7251f5bbcdbe990903cf
Paula <paulasalinor@gmail.com>
2022-07-01 11:48:10 -0300

    feat: add coverage test to getAllSales and getSalesById functions in salesController

--- tests/unit/controllers/salesController.test.js
+++ tests/unit/controllers/salesController.test.js
@@ -3,7 +3,6 @@ const sinon = require('sinon');
 
 const SalesService = require('../../../services/salesServices');
 const SalesController = require('../../../controllers/salesController');
-const { addSalesProducts } = require('../../../models/salesModel');
 
 describe('Test createSales in controller', () => {
   const res = {};
@@ -44,4 +43,76 @@ describe('Test createSales in controller', () => {
     await SalesController.createSales(req, res, next);
     expect(res.json.calledWith(sinon.match.object)).to.be.equal(true);
   });
+});
+
+describe('Test getAllSales function', () => {
+  const req = {}
+  const res = {}
+  const next = () => { }
+
+  const payload = [
+    {
+      "saleId": 1,
+      "date": "2021-09-09T04:54:29.000Z",
+      "productId": 1,
+      "quantity": 2
+    },
+    {
+      "saleId": 1,
+      "date": "2021-09-09T04:54:54.000Z",
+      "productId": 2,
+      "quantity": 2
+    }
+  ]
+  
+  beforeEach(async () => {
+    res.status = sinon.stub().returns(res)
+    res.json = sinon.stub().returns();
+    await sinon.stub(SalesService, 'getAllSales').resolves(payload)
+  });
+
+  afterEach(async () => {
+    await SalesService.getAllSales.restore()
+  });
+
+  it('should be return status 200', async () => {
+    await SalesController.getAllSales(req, res, next)
+    expect(res.status.calledWith(200)).to.be.equal(true);
+  });
+});
+
+describe('Test getSaleById function', () => {
+  const req = {}
+  const res = {}
+  const next = () => { }
+
+  const payload = [
+    {
+      "date": "2021-09-09T04:54:29.000Z",
+      "productId": 1,
+      "quantity": 2
+    },
+    {
+      "date": "2021-09-09T04:54:54.000Z",
+      "productId": 2,
+      "quantity": 2
+    }
+  ]
+
+  beforeEach(async () => {
+    req.params = '1'
+    res.status = sinon.stub().returns(res)
+    res.json = sinon.stub().returns();
+    await sinon.stub(SalesService, 'getSalesById').resolves(payload)
+  });
+
+  afterEach(async () => {
+    await SalesService.getSalesById.restore()
+  });
+
+  it('should be return status 200', async () => {
+    await SalesController.getSalesById(req, res, next)
+    expect(res.status.calledWith(200)).to.be.equal(true);
+  });
+
 });

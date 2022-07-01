b20678e95840e385dea7f348255f8bde0ed2f1ec
Paula <paulasalinor@gmail.com>
2022-07-01 13:19:21 -0300

    feat: add update in productService add update function that check constraints with name and id

--- services/productsService.js
+++ services/productsService.js
@@ -30,8 +30,33 @@ const add = async (name) => {
   return result;
 };
 
+const update = async (name, id) => {
+  if (!name) return { error: { code: 'badRequest', message: '"name" is required' } };
+
+  if (name.length < 5) {
+    return {
+      error: {
+        code: 'invalidDate',
+        message: '"name" length must be at least 5 characters long',
+      },
+    };
+  }
+
+  const product = await ProductsModel.getById(id);
+  
+  if (product.length === 0) return { error: { code: 'notFound', message: 'Product not found' } };
+
+  await ProductsModel.update(id, name);
+
+  return {
+    id,
+    name,
+  };
+};
+
 module.exports = {
   getAll,
   getById,
   add,
+  update,
 };
